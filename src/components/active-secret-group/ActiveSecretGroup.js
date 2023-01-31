fb65535dff6cd3d73d646469c0396a45738136d3
Dustin Keyes <dustin.keyes@gmail.com>
2023-01-31 15:16:14 -0500

    secret notes and styling

--- src/components/active-secret-group/ActiveSecretGroup.js
+++ src/components/active-secret-group/ActiveSecretGroup.js
@@ -13,15 +13,25 @@ export default class ActiveSecretGroup extends React.Component {
 
   render() {
     let secrets;
+    let note;
     if (this.props.activeSecrets) {
-      secrets = this.props.activeSecrets.map((secret, idx) => {
+      secrets = this.props.activeSecrets.map((secretArr, idx) => {
+        let styles = "secret-input";
+        if (idx === this.props.activeSecretIdx) {
+          styles += " active";
+        }
         return (
           <input type="text" key={idx} name={idx}
-            placeholder="type something..."
-            value={secret}
-            onChange={this.props.handleChange} />
+            className={styles} placeholder="type something..." maxLength="60"
+            value={secretArr[0]}
+            onChange={this.props.handleChange}
+            onClick={this.props.handleSecretClick} />
         );
       });
+      note = <textarea placeholder="Add a note..." name="note" maxLength="140" cols="35"
+        className='secret-note'
+        value={this.props.activeSecrets[this.props.activeSecretIdx][1]}
+        onChange={this.props.handleChange} />
     }
 
     return (
@@ -31,9 +41,13 @@ export default class ActiveSecretGroup extends React.Component {
           <button>Edit</button>
           <button>Delete Group</button>
         </div>
-        <div className='secret-list'>
-          {secrets}
+        <div className='secret-info'>
+          <div className='secret-list'>
+            {secrets}
+          </div>
+          {note}
         </div>
+        <button>Save Changes</button>
       </div>
     );
   }
--- src/components/user-secrets/UserSecrets.js
+++ src/components/user-secrets/UserSecrets.js
@@ -7,33 +7,34 @@ export default class UserSecrets extends React.Component {
     super(props)
     this.state = {
       userSecrets: {},
-      activeGroup: ""
+      activeGroup: "",
+      activeSecretIdx: 0
     }
   }
   componentDidMount() {
     this.setState({
       userSecrets: {
         "Family": [
-          "My mom is a witch",
-          "My dad is a wizard",
-          "I am a wizard",
-          "My sister is a witch"
+          ["My mom is a witch", "test note"],
+          ["My dad is a wizard", "blah wizard"],
+          ["I am a wizard", "keep this on the DL"],
+          ["My sister is a witch", "I'm jealous"]
         ],
         "Work": [
-          "My boss is a jerk",
-          "I have a crush on my coworker",
-          "I hate mondays"
+          ["My boss is a jerk", "He snorts cocaine in the bathroom"],
+          ["I have a crush on my coworker", "I can't stop staring when she wears sweaters"],
+          ["I hate mondays", "Mostly casue Francy is there"]
         ],
         "Friends": [
-          "Sarah got an abortion when she was 16",
-          "I have a crush on Sarah",
-          "John killed someone drunk driving",
-          "John hates his wife, sarah",
+          ["Sarah got an abortion when she was 16", "no shame"],
+          ["I have a crush on Sarah", "past mistakes and all"],
+          ["John killed someone drunk driving", "don't know how he sleeps at night"],
+          ["John hates his wife, sarah", "they are such a mess"],
         ],
         "Government": [
-          "I am a spy",
-          "I didn't do my taxes",
-          "I want to overthrow the government"
+          ["I am a spy", "don't rat me out google!"],
+          ["I didn't do my taxes", "but i make now money"],
+          ["I want to overthrow the government", "socialist revolution!"]
         ]
       },
       activeGroup: "Family"
@@ -42,15 +43,24 @@ export default class UserSecrets extends React.Component {
 
   handleChange = (e) => {
     const userSecretsCopy = structuredClone(this.state.userSecrets);
-    const name = e.target.name;
-    userSecretsCopy[this.state.activeGroup][name] = e.target.value;
+    if (e.target.name === "note") {
+      userSecretsCopy[this.state.activeGroup][this.state.activeSecretIdx][1] = e.target.value;
+    } else {
+      const idx = e.target.name;
+      userSecretsCopy[this.state.activeGroup][idx][0] = e.target.value;
+    }
     this.setState({
       userSecrets: userSecretsCopy
     });
   }
+  handleSecretClick = (e) => {
+    const idx = e.target.name;
+    this.setState({activeSecretIdx: idx});
+  }
   makeGroupActive(group) {
     this.setState({
-      activeGroup: group
+      activeGroup: group,
+      activeSecretIdx: 0
     });
   }
 
@@ -77,6 +87,8 @@ export default class UserSecrets extends React.Component {
         </div>
         <ActiveSecretGroup
           handleChange={this.handleChange}
+          handleSecretClick={this.handleSecretClick}
+          activeSecretIdx={this.state.activeSecretIdx}
           activeSecrets={this.state.userSecrets[this.state.activeGroup]}
           activeGroup={this.state.activeGroup} />
       </div>
